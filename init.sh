8bbdef2cf1e029096c806e77ae3405cc53884072
Maxim Mironyuk <serieznyi@gmail.com>
2015-04-04 16:25:37 +0300

    feature: fix init

--- init.sh
+++ init.sh
@@ -8,6 +8,6 @@ chmod 0777 ./humhub/assets/
 
 if [ ! -f ./humhub/protected/config/local/_settings.php ]; then
     touch ./humhub/protected/config/local/_settings.php
-    echo "<?php\n\nreturn [];" >> ./humhub/protected/config/local/_settings.php
+    echo "<?php return [];" >> ./humhub/protected/config/local/_settings.php
     chmod 0777 ./humhub/protected/config/local/_settings.php
 fi
